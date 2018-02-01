8a3fa7c346fe0e485333c49baab90be5f70f95cd
pp1336 <pp1336@users.noreply.github.com>
2018-02-01 17:36:38 +0000

    initial

---
+++ DAPICourseworkSkeleton.py
@@ -0,0 +1,105 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Coursework in Python 
+from DAPICourseworkLibrary import *
+from numpy import *
+#
+# Coursework 1 begins here
+#
+# Function to compute the prior distribution of the variable root from the data set
+def Prior(theData, root, noStates):
+    prior = zeros((noStates[root]), float )
+# Coursework 1 task 1 should be inserted here
+    
+# end of Coursework 1 task 1
+    return prior
+# Function to compute a CPT with parent node varP and xchild node varC from the data array
+# it is assumed that the states are designated by consecutive integers starting with 0
+def CPT(theData, varC, varP, noStates):
+    cPT = zeros((noStates[varC], noStates[varP]), float )
+# Coursework 1 task 2 should be inserte4d here
+   
+# end of coursework 1 task 2
+    return cPT
+# Function to calculate the joint probability table of two variables in the data set
+def JPT(theData, varRow, varCol, noStates):
+    jPT = zeros((noStates[varRow], noStates[varCol]), float )
+#Coursework 1 task 3 should be inserted here 
+    
+# end of coursework 1 task 3
+    return jPT
+#
+# Function to convert a joint probability table to a conditional probability table
+def JPT2CPT(aJPT):
+#Coursework 1 task 4 should be inserted here 
+   
+# coursework 1 taks 4 ends here
+    return aJPT
+
+#
+# Function to query a naive Bayesian network
+def Query(theQuery, naiveBayes): 
+    rootPdf = zeros((naiveBayes[0].shape[0]), float)
+# Coursework 1 task 5 should be inserted here
+  
+
+# end of coursework 1 task 5
+    return rootPdf
+#
+# End of Coursework 1
+#
+# Coursework 2 begins here
+#
+# Calculate the mutual information from the joint probability table of two variables
+def MutualInformation(jP):
+    mi=0.0
+# Coursework 2 task 1 should be inserted here
+   
+
+# end of coursework 2 task 1
+    return mi
+#
+# construct a dependency matrix for all the variables
+def DependencyMatrix(theData, noVariables, noStates):
+    MIMatrix = zeros((noVariables,noVariables))
+# Coursework 2 task 2 should be inserted here
+    
+
+# end of coursework 2 task 2
+    return MIMatrix
+# Function to compute an ordered list of dependencies 
+def DependencyList(depMatrix):
+    depList=[]
+# Coursework 2 task 3 should be inserted here
+    
+
+# end of coursework 2 task 3
+    return array(depList2)
+#
+# Functions implementing the spanning tree algorithm
+# Coursework 2 task 4
+
+def SpanningTreeAlgorithm(depList, noVariables):
+    spanningTree = []
+  
+    return array(spanningTree)
+#
+# End of coursework 2
+#
+
+#
+# main program part for Coursework 1
+#
+noVariables, noRoots, noStates, noDataPoints, datain = ReadFile("Neurones.txt")
+theData = array(datain)
+AppendString("results.txt","Coursework One Results by dfg")
+AppendString("results.txt","") #blank line
+AppendString("results.txt","The prior probability of node 0")
+prior = Prior(theData, 0, noStates)
+AppendList("results.txt", prior)
+#
+# continue as described
+#
+#
+
+
